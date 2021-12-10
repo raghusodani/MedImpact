fcb5dd69f1ac8484b65d72fa7479837245fb40bb
Siddhant <201951151@iiitvadodara.ac.in>
2021-12-11 00:49:41 +0530

    feat:signup

--- medimpact/src/views/Auth/Login/Login.js
+++ medimpact/src/views/Auth/Login/Login.js
@@ -40,7 +40,7 @@ function Login() {
                         <div>
                         <label htmlFor="password" className="sr-only">Password</label>
                         <input id="password" name="password" type="password" autoComplete="current-password" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password"
-                            onChange={(e)=>setPassword(e.target.value)}
+                             onChange={(e)=>setPassword(e.target.value)}
                         />
                         </div>
                     </div>
