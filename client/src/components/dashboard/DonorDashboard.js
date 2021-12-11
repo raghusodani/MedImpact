b5a697c7be5edc67e95c04af11533dc25add5ed6
anuj-2001 <56990215+anuj-2001@users.noreply.github.com>
2021-12-12 04:16:28 +0530

    Donor dash

--- client/src/components/dashboard/DonorDashboard.js
+++ client/src/components/dashboard/DonorDashboard.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import SideNav from './StoreDashboard/SideNav'
+import IntroCard from './StoreDashboard/IntroCard'
 
 function DonorDashboard() {
+    const user = {name:'Anuj',email:'anujpillai1201@gmail.com'}
     return (
-        <div>
-            DonorDashboard
+        <div className="Container">
+            <div class="row">
+                <div class="col-sm-2">
+                    <div className="Content-left">
+                        <SideNav />
+                    </div>
+                </div>
+                <div class="col-sm-10">
+                    <IntroCard user={user}/>
+                    <h1>Hello from donor dashboard</h1>
+                </div>
+            </div>
         </div>
     )
 }
