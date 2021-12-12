0b6e21b92b7d55139a1b59522c5db27f80bb6f1b
anuj-2001 <56990215+anuj-2001@users.noreply.github.com>
2021-12-12 13:00:17 +0530

    new

--- client/src/components/dashboard/DonorDashboard.js
+++ client/src/components/dashboard/DonorDashboard.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import SideNav from './StoreDashboard/SideNav'
 import IntroCard from './StoreDashboard/IntroCard'
+import Card from "react-bootstrap/Card";
+import DonorDashStyling from './DonorDashboard.css'
+import Progress from './DonorDashboard/ProgressBar';
+import TotalDons from './DonorDashboard/TotalDons';
 
 function DonorDashboard() {
     const user = {name:'Anuj',email:'anujpillai1201@gmail.com'}
@@ -14,7 +18,8 @@ function DonorDashboard() {
                 </div>
                 <div class="col-sm-10">
                     <IntroCard user={user}/>
-                    <h1>Hello from donor dashboard</h1>
+                    <TotalDons></TotalDons>
+                    <Progress></Progress>
                 </div>
             </div>
         </div>
---
+++ client/src/components/dashboard/DonorDashboard/TotalDons.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from "react-bootstrap/Card";
+
+function TotalDons() {
+    return (
+        <div>
+            <Card className='total-card'>
+                <h1 className='total-title'>Total Donations : 3</h1>
+            </Card>
+        </div>
+    )
+}
+
+export default TotalDons
